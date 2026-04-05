f81833268b8b87bd0d4a9435ba86319e4599975c
Tõivo Leedjärv <toivol@gmail.com>
2026-04-05 11:55:23 +0200

    Add directory search logic to syslib gettext
    Gettext backend implemented by an external library does not support
    searching for translations from multiple locations and expects to be
    supplied with a single directory. This may not be known at build
    time (also, hard-coding a single directory would not be portable).
    
    Implement translations directory search algorithm that is basically the
    same as the one used by the pure OCaml implementation of gettext. First,
    always share/locale/ within the same prefix as the executable (that is,
    /usr for /usr/bin, /usr/pkg for /usr/pkg/bin, and so on) is attempted.
    If not found then the next priority is the list of default directories
    configured when building ocaml-gettext (this is expected to include the
    system-wide locale directory). Additionally, the list of locale
    directories may be extended by an environment variable (depending on
    version of ocaml-gettext).

--- src/ugettext/ugettext_sys.ml
+++ src/ugettext/ugettext_sys.ml
@@ -18,7 +18,42 @@
 module Ugettext_Config = struct
   let textdomain = "unison"
   let codeset = None (* Allow system gettext to decide *)
-  let dir = Some GettextConfig.default_dir
+  let dir =
+    (* System gettext does not search for MO files in multiple dirs.
+       caml-gettext-stub does not search for MO files either, unlike
+       the pure OCaml implementation, so we have to do the search here.
+       First try the path prefix of the executable, just like with the
+       pure OCaml backend. *)
+    let mo = textdomain ^ ".mo" in
+    let exe =
+      if Filename.is_relative Sys.executable_name then
+        Filename.concat (Sys.getcwd ()) Sys.executable_name
+      else
+        Sys.executable_name in
+    let bindir = Filename.dirname exe in
+    let rootdir = Filename.concat bindir Filename.parent_dir_name in
+    let sharedir = Filename.concat rootdir "share" in
+    let localedir = Filename.concat sharedir "locale" in
+    (* Create a dummy throw-away Gettext module. It is only needed temporarily
+       to use GettextLocale.Posix and to get access to the list of built-in
+       default paths. *)
+    let t = GettextModules.create ~failsafe:Ignore ?codeset textdomain in
+    let (search_locales, _codeset) =
+      GettextLocale.Posix.get_locale t GettextCategory.LC_MESSAGES in
+    let search_dirs = localedir :: t.path in
+    let found_dir =
+      search_dirs |> List.find_opt
+        (fun dir ->
+          search_locales |> List.exists
+            (fun locale ->
+              let fn =
+                Filename.concat (Filename.concat (Filename.concat dir locale)
+                    "LC_MESSAGES") mo in
+              Sys.file_exists fn))
+    in
+    match found_dir with
+    | Some _ -> found_dir
+    | None -> Some GettextConfig.default_dir
   let dependencies = Gettext.init
 end
 
